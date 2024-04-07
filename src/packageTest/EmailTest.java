1151584a4ab0c9392581a284624bc2c560fb33d4
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-04-07 16:26:03 -0300

    se sube version 2 del TP 2 con tarea #TK6 realizada

--- src/packageTest/EmailTest.java
+++ src/packageTest/EmailTest.java
@@ -1,9 +1,18 @@
 package packageTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+import frgp.utn.edu.ar.entidad.Email;
+
 class EmailTest {
+	private String testCorreo;
+	
 	@Test
-	public void testValidarCorreo_almenosUnaMayuscula() {}
+	public void testValidarCorreo_almenosUnaMayuscula() {
+		this.testCorreo = "correo1@email.com";
+		assertEquals(true,Email.validarCorreo(testCorreo), "correo invalido almenos una mayuscula");
+	}
 
 }
