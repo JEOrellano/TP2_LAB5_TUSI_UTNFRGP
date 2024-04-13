f5163678124152b8759ccebcfeab5a28ab35a3ff
Joaquin <gustavo.chazarreta10@gmail.com>
2024-04-13 10:21:16 -0300

    minuscula

--- src/frgp/utn/edu/ar/entidad/Email.java
+++ src/frgp/utn/edu/ar/entidad/Email.java
@@ -6,37 +6,32 @@ import java.util.regex.Pattern;
 public class Email {
 
 	private String correo;
-	
-	public Email(String correo)
-	{
-	   this.correo = correo;
+
+	public Email(String correo) {
+		this.correo = correo;
 	}
 
 	public static boolean validarCorreo(String correo) {
-        String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z0-9]).{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(correo);
-        return matcher.matches();
-    }
-	
-	
-	
+		String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z0-9]).{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(correo);
+		return matcher.matches();
+	}
 
-	
 	public static boolean validarCorreoAlMenosUnNumero_retornaTrue(String correo) {
-        String regex = ".*\\d+.*";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(correo);
-        return matcher.find();
-    }
-	
-	
-	
-	
-	
+		String regex = ".*\\d+.*";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(correo);
+		return matcher.find();
+	}
+	public static boolean validarMinuscula(String correo) {
+		String regex = ".*[a-z].*@.*";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(correo);
+		return matcher.find();
+	}
 	@Override
 	public String toString() {
 		return "Email [correo=" + correo + "]";
 	}
 }
-
